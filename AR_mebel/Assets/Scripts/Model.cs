9dea8dbd6609cc4973b774c2b8be4a8e6511e513
nail361 <nail361@mail.ru>
2018-08-23 17:28:25 +0300

    upd Add Rotate Translate and Scale

--- AR_mebel/Assets/Scripts/Model.cs
+++ AR_mebel/Assets/Scripts/Model.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Model : MonoBehaviour {
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += OnLevelLoaded;
     }
 
-    private void OnLevelWasLoaded()
+    private void OnLevelLoaded(Scene scene, LoadSceneMode mode)
     {
         Transform parent = GameObject.FindGameObjectWithTag("Player").transform;
         if (parent) transform.SetParent(parent);
--- AR_mebel/Assets/Scripts/Player.cs
+++ AR_mebel/Assets/Scripts/Player.cs
@@ -4,13 +4,82 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	private bool holding;
+    private Transform modelTransform;
+ 
+    void Start()
+    {
+        holding = false;
+
+        modelTransform = transform.GetChild(0);
+    }
+ 
+    void Update()
+    {
+ 
+        if (holding)
+        {
+            Move();
+        }
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                if (hit.transform == modelTransform)
+                {
+                    holding = true;
+                }
+            }
+        }
+        else if (Input.GetMouseButtonUp(0)) {
+            holding = false;
+        }
+#else
+        // One finger
+        if (Input.touchCount == 1)
+        {
+         
+            // Tap on Object
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit hit;
+ 
+                if (Physics.Raycast(ray, out hit, 100f))
+                {
+                    if (hit.transform == modelTransform)
+                    {
+                        holding = true;
+                    }
+                }
+            }
+ 
+            // Release
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                holding = false;
+            }
+        }
+#endif
+    }
+
+    void Move()
+    {
+        RaycastHit hit;
+#if UNITY_EDITOR
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+#else
+        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+#endif
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            transform.position = new Vector3(hit.point.x,
+                                             transform.position.y,
+                                             hit.point.z);
+        }
+    }
 }
